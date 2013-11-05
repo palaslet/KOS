2a57f182a3ec8cc3998e4a689bcee3d34db48b71
Pål Sletner <paal.sletner@gmail.com>
2013-11-05 12:15:16 +0100

    Refactoring Sensor system
    Sensor system has been refactored to better support suffixes

--- VesselSensors.cs
+++ VesselSensors.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,56 +7,105 @@ namespace kOS
 {
     public class VesselSensors : SpecialValue
     {
-        Vector acceleration = new Vector(0, 0, 0);
-        Single pressure = 0;
-        Single temperature = 0;
-        Vector geeForce = new Vector(0, 0, 0);
+        private Vessel target;
 
         public VesselSensors(Vessel target)
         {
-            foreach (Part part in target.Parts)
-            {
-                if (part.State == PartStates.ACTIVE || part.State == PartStates.IDLE)
-                {
-                    foreach (PartModule module in part.Modules)
-                    {
-                        if (module is ModuleEnviroSensor)
-                        {
-                            switch (module.Fields.GetValue("sensorType").ToString())
-                            {
-                                case "ACC":
-                                    acceleration = new Vector(FlightGlobals.getGeeForceAtPosition(part.transform.position) - target.acceleration);
-                                    break;
-                                case "PRES":
-                                    pressure = (Single)FlightGlobals.getStaticPressure();
-                                    break;
-                                case "TEMP":
-                                    temperature = part.temperature;
-                                    break;
-                                case "GRAV":
-                                    geeForce = new Vector(FlightGlobals.getGeeForceAtPosition(part.transform.position));
-                                    break;
-                            }
-                        }
-                    }
-                }
-            }
+            this.target = target;
         }
         public override object GetSuffix(string suffixName)
         {
-            if (suffixName == "ACC") return acceleration;
-            if (suffixName == "PRES") return pressure;
-            if (suffixName == "TEMP") return temperature;
-            if (suffixName == "GRAV") return geeForce;
-
-
+            if (suffixName == "LIGHT")
+            {
+                Single totalSunAOA = 0;
+                foreach (Part part in target.Parts)
+                {
+                    if (part.State == PartStates.ACTIVE)
+                    {
+                        foreach (ModuleDeployableSolarPanel c in part.FindModulesImplementing<ModuleDeployableSolarPanel>())
+                        {
+                            totalSunAOA += c.sunAOA;
+                        }
+                    }
+                }
+                return totalSunAOA;
+            }
+            foreach (Part part in target.Parts)
+            {
+                if (part.State == PartStates.ACTIVE)
+                {
+                    foreach (ModuleEnviroSensor sensor in part.FindModulesImplementing<ModuleEnviroSensor>())
+                    {
+                        if (sensor.sensorType == suffixName)
+                            return new VesselSensor(sensor);
+                    }
+                }
+            }
 
             return base.GetSuffix(suffixName);
+        }
+
+        public override bool SetSuffix(string suffixName, object value)
+        {
+            return base.SetSuffix(suffixName, value);
         }
 
         public override string ToString()
         {
             return base.ToString();
+        }
+    }
+
+    public class VesselSensor : SpecialValue
+    {
+        private ModuleEnviroSensor sensor;
+
+        public override object Value
+        {
+            get
+            {
+                return getSensorValue();
+            }
+        }
+
+        public VesselSensor(ModuleEnviroSensor sensor)
+        {
+            this.sensor = sensor;
+        }
+
+        public override object GetSuffix(string suffixName)
+        {
+            if (suffixName == "ACTIVE") return sensor.sensorActive;
+
+            return base.GetSuffix(suffixName);
+        }
+
+        public override bool SetSuffix(string suffixName, object value)
+        {
+            if (suffixName == "ACTIVE")
+            {
+                sensor.sensorActive = (Boolean)value;
+                return true;
+            }
+
+            return base.SetSuffix(suffixName, value);
+        }
+
+        private Object getSensorValue()
+        {
+            switch (sensor.sensorType)
+            {
+                case "ACC":
+                    return new Vector(FlightGlobals.getGeeForceAtPosition(sensor.part.transform.position) - sensor.vessel.acceleration);
+                case "PRES":
+                    return (Single)FlightGlobals.getStaticPressure();
+                case "TEMP":
+                    return sensor.part.temperature;
+                case "GRAV":
+                    return new Vector(FlightGlobals.getGeeForceAtPosition(sensor.part.transform.position));
+                default:
+                    throw new kOSException("Sensor of type '" + sensor.sensorType + "' isn't yet supported by kOS");
+            }
         }
     }
 }
